4124cef8ef61a47422b064a016473e7e387860a6
Alex Barbero <barrufat@hotmail.com>
2023-11-28 17:25:41 +0100

    Add resets & globals

---
+++ src/components/App/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>My Box App</div>;
+};
+
+export default App;
---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "./styled";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    primary: "#db1a00",
+    dark: "#2f2f2f",
+    mediumLight: "#d9d9d9",
+    light: "#fff",
+    backDark: "#131313",
+    backSecondaryDark: "#6843bc",
+    backLight: "#ffffff",
+  },
+  typography: {
+    mainFontFamily: "'Jaldi', serif",
+    contentFontFamily: "'Keania One, serif",
+    mainSize: "32px",
+    principalInfoSize: "24px",
+    detailInfoSize: "20px",
+    infoSize: "22px",
+  },
+};
+
+export default mainTheme;
