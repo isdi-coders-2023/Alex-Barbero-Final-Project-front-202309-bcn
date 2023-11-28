02454985c0121152a0338ac1dbd002fe2ef1a8af
Alex Barbero <barrufat@hotmail.com>
2023-11-28 19:42:27 +0100

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import HeaderStyled from "./HeaderSyled";
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <img
+        src="./resizedLogo.webp"
+        alt="my-box-logo"
+        width="103"
+        height="103"
+        title="my-box-logo"
+      ></img>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
