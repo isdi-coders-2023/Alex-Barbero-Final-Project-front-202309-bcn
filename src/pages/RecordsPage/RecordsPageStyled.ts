93f94bc4301b6973fee0c9deb4ad050da945c094
Alex Barbero <barrufat@hotmail.com>
2023-11-30 01:36:02 +0100

    Add main title styles

--- src/pages/RecordsPage/RecordsPageStyled.ts
+++ src/pages/RecordsPage/RecordsPageStyled.ts
@@ -5,6 +5,24 @@ const RecordsPageStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  .main-title {
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+    background: linear-gradient(
+      180deg,
+      #fff 69.1%,
+      rgba(255, 255, 255, 0.54) 87.33%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    color: #db1a00;
+    text-transform: uppercase;
+    width: 100%;
+    height: 50px;
+    position: fixed;
+    top: 90px;
+    z-index: 3;
+    text-align: center;
+  }
 `;
 
 export default RecordsPageStyled;
