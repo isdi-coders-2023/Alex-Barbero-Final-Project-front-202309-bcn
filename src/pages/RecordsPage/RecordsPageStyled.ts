65876ab27bc8dff042cc1567e44eaaf0bee54b5f
Alex Barbero <barrufat@hotmail.com>
2023-12-08 12:13:02 +0100

    Add RecordsDetailsPageStyled

--- src/pages/RecordsPage/RecordsPageStyled.ts
+++ src/pages/RecordsPage/RecordsPageStyled.ts
@@ -6,16 +6,6 @@ const PageStyled = styled.div`
   flex-direction: column;
   align-items: center;
 
-  .toast {
-    position: fixed;
-    top: 120px;
-    z-index: 4;
-    padding: 10px;
-    background-color: #b0b0b0;
-    border: black;
-    border-radius: 10px;
-  }
-
   .main-title {
     font-family: fantasy;
     font-weight: lighter;
